64fde0be3c93d432b2cd820a85bf912b600658c3
Trisha Barua <152303377+TrishaBarua308@users.noreply.github.com>
2026-09-15 01:12:51 +0600

    Add files via upload

---
+++ targetPracticeAgain.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    optimize();
+    int a[10][10] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    int t; cin>>t;
+    while(t--)
+    {
+        char ch[10][10];
+        int ans=0;
+        for(int i=0; i<10; i++)
+        {
+            for(int j=0; j<10; j++)
+            {
+                cin>>ch[i][j];
+                if(ch[i][j]=='X')
+                {
+                    ans += a[i][j];
+                }
+            }
+        }
+
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
